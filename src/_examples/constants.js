cdeae61ac086f47e685c0544fd03e04da739496a
codebender828 <jbakebwa@163.com>
2020-12-08 19:16:16 +0800

    refactor: reset and removed untracked files

--- src/_examples/constants.js
+++
@@ -1,6 +0,0 @@
-exports.TRANSLATION_SERVICE = {
-  KEY: 'fcd4e311f1784c7abef20560b8d07047',
-  ENDPOINT: 'https://api.cognitive.microsofttranslator.com',
-  LOCATION: 'eastasia',
-  VERSION: '3.0'
-}
